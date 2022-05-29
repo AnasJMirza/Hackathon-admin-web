79b48bae3046cf96392a15f13f61d9bf1fa73fb4
AnasJMirza <ajmhimself@gmail.com>
2022-05-30 02:37:15 +0500

    works on forms

--- src/components/pages/addProductsForm/AddProductsForm.js
+++ src/components/pages/addProductsForm/AddProductsForm.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Button from '../../button/Button';
 import './AddProductsForm.css'
 
 const AddProductsForm = () => {
+
+    let productTitle = "";
+    let productPrice = "";
+
+    const titleGetter = (e) => {
+        productTitle = e.target.value
+    }
+
+    const priceGetter = (e) => {
+        productPrice = e.target.value
+    }
+
+
     return (
         <div className='form-body'>
             <form action='#'>
-                <input type="text" placeholder='Title' />
+                <input  onChange={(e)=>titleGetter(e)} type="text" placeholder='Prodcut Title' />
+                <input  onChange={(e)=>priceGetter(e)} type="number" placeholder='Product Price' />
+                <div>
+                    <Button title="Submit" width="10vw" />
+                </div>
             </form>
         </div>
     );
